1c617aeb2f9daad070a582ed242bb63206f2bc12
erardlucien <erardlaurent1@gmail.com>
2023-09-25 23:02:10 +0200

    The animation is now longer repeat itself.

--- js/index.js
+++ js/index.js
@@ -1,17 +1,11 @@
 let textContainer = document.querySelector('.text-container');
 let text = document.querySelector('.text');
 let counter = 0;
-let timeout;
-let timeout2;
 
 function countdown() {
 
     if(textContainer.classList.contains('text-container-active') && counter < 100) {
         text.textContent = `${ counter += 10 }%`;
-        if(counter === 100) {
-            text.textContent = 'I am happy.';
-            counter = 0;
-        }
         timeout = setTimeout(countdown, 300); 
     } else {
         clearTimeout(timeout);
@@ -19,18 +13,7 @@ function countdown() {
 
 }
 
-(
-    function active() {
-
-        setTimeout( () => {
-            textContainer.classList.toggle('text-container-active');
-            if(textContainer.classList.contains('text-container-active')) {
-                timeout2 = setTimeout(countdown, 100);
-            } else {
-                clearTimeout(timeout2);
-            }          
-        }, 100);
-
-        setTimeout(active, 3000);
-    }
-)();
+setTimeout( () => {
+    textContainer.classList.add('text-container-active');
+    setTimeout(countdown, 100);          
+}, 100);
